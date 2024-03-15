31811950e0758f11ea1e5e5855677f74d0e73096
Libon <bon.li@outlook.com>
2024-03-15 20:46:29 +0800

    style: 方块样式调整

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -12,7 +12,7 @@ export function Toggle({
   return (
     <label className={cls('block select-none', color, className)}>
       <input type="checkbox" className="peer hidden" {...rest} />
-      <i className="inline-block w-full h-full rounded-md bg-current border-[rgba(0,0,0,.2)] border border-b-4 text-white peer-checked:text-inherit peer-checked:border-b-[1px] peer-checked:translate-y-0.5" />
+      <i className="inline-block w-full h-full rounded-lg text-white bg-current border-[rgba(0,0,0,.2)] peer-checked:text-inherit peer-checked:bg-current peer-checked:shadow-[0_0_8px_currentColor]" />
     </label>
   )
 }
