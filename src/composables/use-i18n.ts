e1401f0058a4ca760435fde705ab4ed8c0953f54
Libon <bon.li@outlook.com>
2024-04-08 16:40:23 +0800

    chore: 去除多余函数调用，取消多语言的响应式

--- src/composables/use-i18n.ts
+++ src/composables/use-i18n.ts
@@ -12,34 +12,24 @@ interface I18N {
 
 export const i18NInjectionKey = Symbol('i18n') as InjectionKey<I18N>
 
-function loadLanguage(lang: Language) {
-  return import.meta.glob(`@/locale/*.json`)[`/src/locale/${lang}.json`]()
-}
-
 export function useI18N() {
   const lang = shallowRef<Language>(getLanguage() as Language)
 
-  const messages = ref<Record<Language, null | Record<string, string>>>({
+  const messages = <Record<Language, Record<string, string>>>{
     'zh-CN': zhCN,
     'en-US': enUS,
     'ja-JP': jaJP,
-  })
+  }
 
-  const msg = computed(() => messages.value[lang.value] ?? messages.value['zh-CN'])
+  const msg = computed(() => messages[lang.value] ?? messages['zh-CN'])
 
   const _setLang = (language: Language) => {
     lang.value = language
     setLanguage(language)
-
-    if (!messages.value[language]) {
-      loadLanguage(language).then((res: any) => {
-        messages.value[language] = res.default as Record<string, string>
-      })
-    }
   }
 
   const $t = (key: string, fallback: string = ''): string => {
-    return msg.value?.[key] ?? fallback
+    return msg.value[key] ?? fallback
   }
 
   _setLang(lang.value)
