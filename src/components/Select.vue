d92a7c8ac012b2bfec2d8be6c76c118d85a74135
Libon <bon.li@outlook.com>
2024-04-09 12:50:58 +0800

    style: 修正暗色模式下语言选择框样式不正确的问题

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -26,21 +26,21 @@ const modelValue = defineModel < string > ()
 <template>
   <Listbox v-model="modelValue" as="div" class="relative h-10">
     <ListboxButton>
-      <span class="inline-flex items-center justify-center px-3 h-[31px] select-none rounded-lg text-sm shadow-sm border-[rgba(0,0,0,.2)] border-x-[1.5px] border-t-[1.5px] border-b-4 active:border-b-[1.5px] active:h-[26px] active:translate-y-1 active:shadow-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-1">
+      <span class="inline-flex items-center justify-center px-3 h-[31px] select-none rounded-lg text-sm shadow-sm dark:bg-gray-700 border-[rgba(0,0,0,.2)] border-x-[1.5px] border-t-[1.5px] border-b-4 active:border-b-[1.5px] active:h-[26px] active:translate-y-1 active:shadow-none focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-1">
         {{ optionsMap[modelValue!] }}
       </span>
     </ListboxButton>
 
     <transition leave-active-class="transition duration-100 ease-in" leave-from-class="opacity-100" leave-to-class="opacity-0">
       <ListboxOptions
-        class="absolute top-9 right-0 max-h-60 min-w-20 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
+        class="absolute top-9 right-0 max-h-60 min-w-20 w-full overflow-auto rounded-md bg-[hsl(var(--background))] border-input py-1 text-base shadow-lg ring-1 ring-black/5 dark:ring-white/10 focus:outline-none sm:text-sm"
       >
         <ListboxOption v-for="option in options" v-slot="{ active, selected }" :key="option.value" :value="option.value" as="template">
           <li
             class="relative cursor-default select-none px-2 py-1.5 text-center truncate [&+&]:mt-0.5"
             :class="[
-              active ? 'bg-gray-50' : '',
-              selected ? '!bg-gray-100' : '',
+              active ? 'bg-gray-50 dark:!bg-gray-900' : '',
+              selected ? '!bg-gray-100 dark:!bg-gray-900' : '',
             ]"
           >
             {{ option.label }}
