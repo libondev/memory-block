9319c33fd4c75ad821ad29cbd97fa0811400b9e5
laihanbin <512412344@qq.com>
2024-04-10 15:07:41 +0800

    fix: nav header 语言按钮切换时未存储到本地

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { gameSoundsInjectionKey } from '@/composables/use-game-sounds'
 import { i18NInjectionKey } from '@/composables/use-i18n'
+import type { Language } from '@/composables/use-local-cache'
 import { languages } from '@/config/game'
 
 const {
@@ -8,7 +9,7 @@ const {
   toggleSounds,
 } = inject(gameSoundsInjectionKey)!
 
-const { lang, $t } = inject(i18NInjectionKey)!
+const { lang, setLanguage, $t } = inject(i18NInjectionKey)!
 </script>
 
 <template>
@@ -20,7 +21,7 @@ const { lang, $t } = inject(i18NInjectionKey)!
     </h1>
 
     <div class="pt-2 flex items-center gap-2">
-      <Select v-model="lang" :options="languages" />
+      <Select v-model="lang" :options="languages" @update:model-value="(e: unknown) => setLanguage(e as Language)" />
 
       <Button @click="toggleSounds()">
         <i class="block" :class="enableSounds ? 'i-solar-volume-loud-broken' : 'i-solar-volume-cross-broken text-red-500'" />
