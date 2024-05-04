4d4f79c20766a82f8fa400198e04687a83de9ff1
libondev <bon.li@outlook.com>
2024-05-04 13:53:00 +0800

    feat: 流浪商人打折

--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -15,6 +15,20 @@ const gameGoods = ref<GameGood[]>([])
 
 const gameMoney = shallowRef(0)
 
+// 商人倒计时
+const comingCountdown = computed(() => {
+  const now = new Date()
+
+  // 如果已经是星期六和星期天则返回 0
+  if ([0, 6].includes(now.getDay())) {
+    return 0
+  }
+
+  const nextWeekend = new Date(now.getFullYear(), now.getMonth(), now.getDate() + (6 - now.getDay()))
+
+  return Math.ceil((nextWeekend.getTime() - now.getTime()) / (1000 * 60 * 60 * 24))
+})
+
 function buyGood(good: typeof GAME_GOODS[number]) {
   if (gameMoney.value < good.price) {
     useToast('金币不足')
@@ -22,7 +36,10 @@ function buyGood(good: typeof GAME_GOODS[number]) {
     return
   }
 
-  gameMoney.value -= good.price
+  // 商人来了就使用折扣价, 否则原价
+  const goodPrice = comingCountdown.value ? good.price : good.price * good.discountPrice
+
+  gameMoney.value -= goodPrice
   setGameMoney(gameMoney.value)
 
   playSuccessSound()
@@ -75,20 +92,34 @@ onMounted(() => {
 
 <template>
   <div class="h-full w-full max-w-[700px] px-4 mx-auto overflow-auto">
-    <div class="w-full flex items-center justify-center mt-12 mb-8 font-mono">
+    <div class="w-full flex items-center justify-center mt-16 font-mono">
       <i class="i-solar-chat-round-money-bold mr-1.5 text-3xl text-yellow-400" />
-      <Increase :value="gameMoney" class="text-[45px]" color="text-red-500" />
+      <Increase :value="gameMoney" class="text-[45px] font-medium" color="text-red-500" />
     </div>
 
-    <ul class="flex flex-wrap items-center gap-4 w-full">
+    <div v-if="comingCountdown" class="text-center text-xl my-6">
+      流浪商人还有 <span class="font-bold text-emerald-500">{{ comingCountdown }}</span> 天抵达...
+    </div>
+
+    <div v-else class="mb-6 pl-2 relative">
+      <img src="@/assets/merchant.png" class="h-40" alt="">
+
+      <div class="absolute left-32 right-4 top-3 sm:top-8 p-2 rounded-lg border border-input">
+        你好，我是<span class="line-through opacity-30">(流浪)</span>旅行至此的商人，我每周末都会为你带来更低的价格<span class="line-through opacity-30">(别问为什么，问就是老板是我表哥)</span>，随便看看吧！<span class="line-through opacity-30">(商品滞销帮帮我们)</span>
+      </div>
+    </div>
+
+    <ul class="flex flex-wrap items-center gap-4 w-full font-medium">
       <li v-for="good of gameGoods" :key="good.id" class="flex w-full rounded-lg p-2 sm:w-80 overflow-hidden dark:border-white/20 border">
         <div class="min-w-[100px] w-[100px] h-[100px] flex items-center justify-center relative before:absolute before:rounded-md before:inset-0 before:bg-current before:z-0 before:opacity-15" :class="good.color">
           <i :class="good.icon" class="text-6xl relative z-10" />
 
-          <span class="absolute bottom-0.5 right-0.5 z-10 select-none flex items-center text-sm font-medium rounded-sm px-1 text-foreground bg-white/70 dark:bg-white/20">
-            <i class="i-solar-chat-round-money-bold mr-0.5 text-yellow-400" />
-            <span>{{ good.price }}</span>
-            <!-- <span>{{ good.discountPrice }}</span> -->
+          <span class="absolute bottom-0.5 right-0.5 z-10 select-none flex items-center text-sm rounded-sm px-1 text-foreground bg-white/70 dark:bg-white/20">
+            <i class="i-solar-chat-round-money-bold size-[14px] text-yellow-400" />
+            <span
+              :data-original="good.price"
+              :class="{ 'before:inline-block before:-mx-0.5 before:content-[attr(data-original)] before:text-red-500 before:line-through before:scale-75': !comingCountdown }"
+            >{{ comingCountdown ? good.price : good.price * good.discountPrice }}</span>
           </span>
         </div>
 
@@ -107,7 +138,7 @@ onMounted(() => {
 
           <div class="flex items-center justify-between gap-2 select-none">
             <button
-              class="flex items-center justify-center w-full text-sm rounded-md py-1 px-2 text-white font-medium bg-emerald-500 active:bg-emerald-600 dark:bg-emerald-600 dark:active:bg-emerald-700 shadow-sm cursor-pointer"
+              class="flex items-center justify-center w-full text-sm rounded-md py-1 px-2 text-white font-medium bg-emerald-500 active:bg-emerald-600 dark:bg-emerald-600 dark:active:bg-emerald-700 outline-none shadow-sm cursor-pointer focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-1"
               @click="buyGood(good)"
             >
               <i class="i-solar-bag-3-bold-duotone translate-y-[1px] text-lg mr-1" />
